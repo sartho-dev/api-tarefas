cabf0b9f772263e3e1d3d067f1da1c36832bd769
sartho-dev <sartho2024@gmail.com>
2025-05-30 23:26:31 -0400

    Mudando pra query

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,17 @@ import "dotenv/config";
 import express from "express";
 import { router } from "./routes/router";
 
-import session from "express-session";
+import cors from "cors"
+
+
 
 //express() é uma função que retorna um objeto
 const app = express();
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}))
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
