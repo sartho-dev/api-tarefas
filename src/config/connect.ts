454e33f96cce077b193a098dc7687ec2f717e3b6
sartho-dev <sartho2024@gmail.com>
2025-05-31 03:24:23 -0400

    Mudando pra query

--- src/config/connect.ts
+++ src/config/connect.ts
@@ -5,8 +5,11 @@ const db = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
-  
+  port: process.env.DB_PORT,
+  ssl: process.env.NODE_ENV == "production"?{
+    rejectUnauthorized: false,
+    requestCert: true
+  }:undefined
 });
 
 export { db };
